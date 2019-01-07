e79519233b23ccab20ba2f8f957a391c16b5e7e0
Cheng Vue <cvu3@ucsc.edu>
2019-01-07 04:03:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,9 +27,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    @user = User.find_by(email: params["email"], password: params["password"])
-    session[:id] = @user.id
-    redirect '/users/home'
+    if !!User.find_by(email: params["email"], password: params["password"])
+      @user = User.find_by(email: params["email"], password: params["password"])
+      session[:id] = @user.id
+      redirect '/users/home'
+    else
+      redirect '/sessions/login'
+    end
   end
 
   get '/sessions/logout' do
